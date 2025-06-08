f96db887495161e71cc893ec1b8bf79cfcbaeb44
Bhavik Vala <bhavik.vala@yahoo.com>
2025-06-08 06:50:19 +0530

    edit order black screen fix2

--- pages/order/[orderId].js
+++ pages/order/[orderId].js
@@ -1,3 +1,4 @@
+import React from 'react'; // Added to fix ReferenceError
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { supabase } from '../../lib/supabase';
